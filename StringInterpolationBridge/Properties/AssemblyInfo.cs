9425259f56921c5ff673d72e3af4de01c8b870c6
Andrey Shchekin <ashmind@gmail.com>
2015-02-20 22:16:33 +1300

    Tested/updated version.

--- StringInterpolationBridge/Properties/AssemblyInfo.cs
+++ StringInterpolationBridge/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion(AssemblyInfo.VersionString)]
 [assembly: AssemblyFileVersion(AssemblyInfo.VersionString)]
-[assembly: AssemblyInformationalVersion(AssemblyInfo.VersionString + "-untested")]
+[assembly: AssemblyInformationalVersion(AssemblyInfo.VersionString)]
 
 // ReSharper disable once CheckNamespace
 internal static class AssemblyInfo {
